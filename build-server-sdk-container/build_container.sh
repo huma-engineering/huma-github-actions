e64b7853f4a62e51669f3bdc9f9be6dedcaceb3d
Denis Trofimov <denis.trofimov@huma.com>
2022-06-17 10:46:58 +0300

    feat: [HCB-424] Build container action
    Signed-off-by: Denis Trofimov <denis.trofimov@huma.com>

---
+++ build-server-sdk-container/build_container.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+export DOCKER_BUILDKIT=1
+short_sha=$(git rev-parse --short HEAD)
+github_token=$1
+github_user=$2
+tag=$3
+repository=${4:-"ghcr.io/huma-engineering/py-ppserver"}
+registry=${5:-"ghcr.io"}
+source=${6:-"https://github.com/huma-engineering/py-phoenix-server"}
+dockerfile=${7:-"docker/ppserver/Dockerfile"}
+
+echo ${github_token} | \
+     ${registry} -u ${github_user} --password-stdin
+docker build . --cache-from ${repository}:latest \
+    -f ${dockerfile} \
+    -t ${repository}:${tag} \
+    --build-arg=GIT_COMMIT=${short_sha} \
+    --label commit=${short_sha} \
+    --label org.opencontainers.image.source=${source}
+docker push ${repository}:${tag}
