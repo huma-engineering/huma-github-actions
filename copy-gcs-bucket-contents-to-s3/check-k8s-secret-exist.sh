bf84b476f17b8be3559df182923cde3940dbd527
Denis Trofimov <denis.trofimov@huma.com>
2022-06-23 11:42:46 +0300

    ref: use env in the copy bucket action [skip ci]
    Signed-off-by: Denis Trofimov <denis.trofimov@huma.com>

--- copy-gcs-bucket-contents-to-s3/check-k8s-secret-exist.sh
+++ copy-gcs-bucket-contents-to-s3/check-k8s-secret-exist.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 for i in $(seq 1 60)
 do
-    [ $i -gt 1 ] && echo "not found secret $1/$2, attempt #$i" && sleep 3
-    SECRET=$(kubectl get -n $2 secrets/$1 -o name --ignore-not-found) \
+    [ $i -gt 1 ] && echo "not found secret ${secret_name}/${namespace}, attempt #$i" && sleep 3
+    SECRET=$(kubectl get -n ${namespace} secrets/${secret_name} -o name --ignore-not-found) \
         && [ -n "${SECRET}" ] && s=0 && break || s=1
 done
 (exit $s)
---
+++ copy-gcs-bucket-contents-to-s3/copy-gcs-bucket-contents-to-s3.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+aws configure set default.s3.max_concurrent_requests 50
+mkdir ${DIR}
+for path in $(gsutil ls gs://${gcs_bucket})
+do
+    echo "downloading each item from GCS bucket path=${path} to the folder=${DIR}"
+
+    for i in $(seq 1 3)
+    do
+        [ $i -gt 1 ] && echo "Recovered from an error, attempt #$i" && sleep 15
+        gsutil -m -q cp -r -U -c -L cp.log ${path} ${DIR} \
+            && s=0 && break || s=$?
+    done
+    # exit if gsutil copy is not successful
+    [ $s -gt 0 ] && (exit $s)
+
+    echo "uploading each item from the folder=${DIR} to the S3 bucket s3://${s3_bucket}"
+    aws s3 cp --recursive --only-show-errors ${DIR} s3://${s3_bucket}
+    echo "clean up all files from the folder=${DIR} to preserve disk space"
+    rm -rf ${DIR}/*
+done
+echo "Post Configure AWS Credentials clean up"
+rm ~/.aws/credentials
